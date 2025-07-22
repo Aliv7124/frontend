6cbc4700432c6021756e461e0937972f504d87ba
Aliv7124 <alivroy25@gmail.com>
2025-07-22 19:38:44 +0530

    Update host to deployed backend

--- src/Context/Notes/NoteState.jsx
+++ src/Context/Notes/NoteState.jsx
@@ -4,7 +4,7 @@ import { useState} from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host = "https://inotebook-backend-k14d.onrender.com";
   const [notes, setNotes] = useState([]);
 
   // Get all notes
