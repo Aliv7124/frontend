9f13e6ccedd84d864887d7cdbc977697cc86039a
Aliv7124 <alivroy25@gmail.com>
2025-07-22 19:50:23 +0530

    Update frontend and fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import NoteState from './Context/Notes/noteState';
+import NoteState from './Context/Notes/NoteState';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NoteState>
-      <BrowserRouter>
+      <BrowserRouter  basename="/frontend">
         <App />
       </BrowserRouter>
     </NoteState>
